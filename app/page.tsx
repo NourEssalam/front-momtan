2490f4685f13296cda3c71f33389614ebff457d8
NourEssalam <werteni11thug@gmail.com>
2024-10-10 21:04:45 +0100

    add contact page and fix feaw pages

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <Partner />
       <AboutNutshell />
-      <EventUpcoming />
+      {/* <EventUpcoming /> */}
       <SignUp />
     </main>
   );
--- lib/form-schema.ts
+++ lib/form-schema.ts
@@ -8,3 +8,16 @@ export const newsLetterSchema = z.object({
     .min(1, { message: "Please enter your email address." }),
   option: z.string().min(1, { message: "Please choose one option." }),
 });
+
+export const contactSchema = z.object({
+  name: z.string().trim().min(3, { message: "Please enter your name." }),
+  email: z
+    .string()
+    .email({ message: "Please enter a valid email address." })
+    .min(1, { message: "Please enter your email address." }),
+  message: z
+    .string()
+    .min(10, {
+      message: "Please enter a message with at least 10 characters.",
+    }),
+});
